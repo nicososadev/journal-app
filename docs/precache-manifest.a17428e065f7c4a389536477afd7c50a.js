6eaa4d5a905ec0e0d7eb2bb627c036f8f24d8070
nicososadev <sosanicolas.dev@gmail.com>
2020-10-03 19:35:19 -0300

    Docs preparado para produccion

---
+++ docs/precache-manifest.a17428e065f7c4a389536477afd7c50a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78965b75cbe86077867a44d6682cdec3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "053eee61d2dc0354929b",
+    "url": "/static/css/main.b16cd110.chunk.css"
+  },
+  {
+    "revision": "c3bb3c573f8a72a5cb8b",
+    "url": "/static/js/2.49fdfeab.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/static/js/2.49fdfeab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "053eee61d2dc0354929b",
+    "url": "/static/js/main.d3323630.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
